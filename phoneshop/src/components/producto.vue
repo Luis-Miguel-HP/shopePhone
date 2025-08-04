030f87ad7a6c93e4084883875fab4da0e379083f
LuisMhp <119384750+Luis-Miguel-HP@users.noreply.github.com>
2025-08-03 22:35:12 -0400

    agregando componentes02

---
+++ phoneshop/src/components/producto.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+<template>
+
+       <div class="flex gap-6">
+      <div>
+        <img class="w-48 h-auto rounded-lg" src="../public/img/iphone 15.webp" alt="iPhone 15">
+      </div>
+      <div class="flex flex-col gap-5 w-[200px]">
+        <h2 class="text-xl font-bold">iPhone 15</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+        <p class="text-2xl text-blue-700 font-bold">$300</p>
+        <button class="inline-block text-white font-bold bg-black font-semibold  text-sm  px-5 py-3  rounded-lg">AGREGAR AL CARRITO</button>
+      </div>
+    </div>
+
+</template>
+
+
+
+
+<style scoped>
+</style>
