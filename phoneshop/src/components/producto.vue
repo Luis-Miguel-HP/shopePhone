c657b1bb39791b050610091ef71d236c63086a91
LuisMhp <119384750+Luis-Miguel-HP@users.noreply.github.com>
2025-08-04 21:09:49 -0400

    agregando componentes04

--- phoneshop/src/components/producto.vue
+++ phoneshop/src/components/producto.vue
@@ -10,15 +10,16 @@ const {producto} = defineProps({
 </script>
 <template>
 
-       <div class="flex gap-6">
+       <div class="ml-10 flex gap-3 mb-10">
       <div>
-        <img class="w-48 h-auto rounded-lg" src="" :alt="' imagen' +  producto.imagen">
+        <img class="w-48 h-[200px] rounded-lg" :src="'/img/Telefonos/'+producto.imagen+'.webp'" :alt="'imagen ' +  producto.imagen">
+
       </div>
       <div class="flex flex-col gap-5 w-[200px]">
         <h2 class="text-xl font-bold">{{ producto.nombre }}</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+        <p>{{ producto.descripcion }}</p>
         <p class="text-2xl text-blue-700 font-bold">$300</p>
-        <button class="inline-block text-white font-bold bg-black font-semibold  text-sm  px-5 py-3  rounded-lg">AGREGAR AL CARRITO</button>
+        <button class="inline-block text-white font-bold bg-blue-500 font-semibold  text-sm  px-5 py-3  rounded-lg">AGREGAR AL CARRITO</button>
       </div>
     </div>
 
--- phoneshop/src/db/db.ts
+++ phoneshop/src/db/db.ts
@@ -11,42 +11,42 @@ export const productos: Producto[] = [
     id: 1,
     nombre: "iPhone 16",
     descripcion: "El último iPhone con chip A18 Pro, cámara de 48 MP, pantalla ProMotion de 120Hz y batería de larga duración.",
-    imagen: "telefono-1",
+    imagen: "telefono1",
     precio: 1099
   },
   {
     id: 2,
     nombre: "iPhone 15",
     descripcion: "Cuenta con chip A16 Bionic, cámara mejorada con modo noche, USB-C y diseño resistente al agua.",
-    imagen: "telefono-2",
+    imagen: "telefono2",
     precio: 899
   },
   {
     id: 3,
     nombre: "iPhone 14",
     descripcion: "Equipado con chip A15 Bionic, cámaras duales con Photonic Engine y detección de choques.",
-    imagen: "telefono-3",
+    imagen: "telefono3",
     precio: 699
   },
   {
     id: 4,
     nombre: "iPhone 13",
     descripcion: "Procesador A15 Bionic, cámara mejorada con modo cinemático y excelente duración de batería.",
-    imagen: "telefono-4",
+    imagen: "telefono4",
     precio: 599
   },
   {
     id: 5,
     nombre: "iPhone 12",
     descripcion: "Pantalla OLED, conectividad 5G y chip A14 Bionic. Diseño plano con Ceramic Shield.",
-    imagen: "telefono-5",
+    imagen: "telefono5",
     precio: 499
   },
   {
     id: 6,
     nombre: "iPhone 11",
     descripcion: "Gran rendimiento con chip A13 Bionic, cámara dual ultra gran angular y batería confiable.",
-    imagen: "telefono-6",
+    imagen: "telefono6",
     precio: 399
   }
 ];
